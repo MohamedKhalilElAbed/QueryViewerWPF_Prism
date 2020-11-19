fd09bac1e2f9d347d732a2d98f4206a307fff9bf
mohamed Khalil El Abed <mohamed.khalil.el.abed@gmail.com>
2020-11-19 11:02:52 +0100

    OnPropertyChanged

--- Client.Application/obj/Debug/netcoreapp3.1/App.g.cs
+++ Client.Application/obj/Debug/netcoreapp3.1/App.g.cs
@@ -47,7 +47,7 @@ namespace Client.Application {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.0.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -67,7 +67,7 @@ namespace Client.Application {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.0.0")]
         public static void Main() {
             Client.Application.App app = new Client.Application.App();
             app.InitializeComponent();
